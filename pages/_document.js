52d04e046e16dca7f2a99d54027f19853bfff898
SalesCentriAi <salescentriai@gmail.com>
2025-08-15 16:02:26 +0530

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -4,13 +4,17 @@ export default function Document() {
   return (
     <Html>
       <Head>
-<!-- Google Tag Manager -->
-<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id=GTM-KT8CFBQT'+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-KT8CFBQT');</script>
-<!-- End Google Tag Manager -->
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id=GTM-KT8CFBQT'+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-KT8CFBQT');`
+          }}
+        />
+        {/* End Google Tag Manager */}
       </Head>
       <body>
         <Main />
